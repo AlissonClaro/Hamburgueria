b6162379713f3aabe75b4bb8a4234a0c6ae65910
AlissonClaro <allnclaro@outlook.com>
2024-07-12 10:34:37 -0300

    build scripts

---
+++ script.js
@@ -0,0 +1,96 @@
+const menu = document.getElementById('menu')
+const cartBtn = document.getElementById('cart-btn')
+const cartModal = document.getElementById('cart-modal')
+const cartItemsContainer = document.getElementById('cart-items')
+const cartTotal = document.getElementById('cart-total')
+const checkOut = document.getElementById('checkout-btn')
+const closeModalBtn = document.getElementById('close-modal-btn')
+const cartCounter = document.getElementById('cart-count')
+const addressInput = document.getElementById('address')
+const addressWarn = document.getElementById('address-warn')
+
+let cart = [];
+
+
+cartBtn.addEventListener('click', () => {
+    updateCartModal()
+    cartModal.style.display = 'flex'
+})
+
+cartModal.addEventListener('click', (event) => {
+    if (event.target === cartModal) {
+        cartModal.style.display = 'none'
+    }
+})
+
+closeModalBtn.addEventListener('click', () => {
+    cartModal.style.display = 'none'
+})
+
+menu.addEventListener('click', (event) => {
+
+    let parentButton = event.target.closest('.add-to-cart-btn')
+
+    if (parentButton) {
+        const name = parentButton.getAttribute('data-name')
+        const price = parseFloat(parentButton.getAttribute('data-price')).toFixed(2)
+
+        addToCart(name, price)
+
+        // adicionar no carrinho
+    }
+
+})
+
+
+// Função para adicionar no carrinho.
+function addToCart(name, price) {
+
+    const existingItem = cart.find(item => item.name === name)
+
+    if (existingItem) {
+        // se o item ja existe, aumenta apenas a quantidade + 1
+        existingItem.qtd += 1;
+
+    } else {
+
+        cart.push({
+            name,
+            price,
+            qtd: 1,
+        })
+    }
+
+    updateCartModal()
+
+}
+
+// atualizar o carrinho
+function updateCartModal() {
+    cartItemsContainer.innerHTML = ''
+    let total = 0
+
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement('div')
+        cartItemElement.classList.add('flex', 'justify-between', 'mb-4', 'flex-col')
+
+        cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between">
+            <div>
+                <p class='font-bold'>${item.name}</p>
+                <p>${item.qtd} un.</p>
+                <p class='text-gray-600 font-medium mt-2'>R$ ${item.price} cada</p>
+            </div>
+
+            <button>
+                Remover
+            </button>
+
+        </div>
+        `
+
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+
+}
